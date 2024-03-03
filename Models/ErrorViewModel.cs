c1086d16b8beb0f5d792df4a5f0bc6ee09209ebf
Wiszowaty Kacper <s10393kw@ms.wwsi.edu.pl>
2024-03-03 11:19:05 +0100

    add FilmDB and FilmController
    git push origin main
    exit
    gds
    gs
    fds
    fd
    fdsfs
    fsd
    f
    sdf
    sd
    fds
    end

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FilmDB1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/FilmDB.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmDB1.Models
+{
+    public class FilmDB
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public int year { get; set; }
+
+    }
+}
